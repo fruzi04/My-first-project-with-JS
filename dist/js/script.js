2fc4057f42eda09edabf51dbbb3d058aa5ec66e1
fruzi04 <yakerich228@gmail.com>
2024-03-19 15:12:37 +0200

    timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -39,13 +39,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Timer
 
-  const deadline = '2020-05-11';
+  const deadline = '2022-06-11';
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()),
       days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor(t / (1000 * 60 * 60) % 24),
+      seconds = Math.floor(t / 1000 % 60),
       minutes = Math.floor(t / 1000 / 60 % 60),
-      seconds = Math.floor(t / 1000 % 60);
+      hours = Math.floor(t / (1000 * 60 * 60) % 24);
     return {
       'total': t,
       'days': days,
@@ -54,19 +54,27 @@ window.addEventListener('DOMContentLoaded', () => {
       'seconds': seconds
     };
   }
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return '0' + num;
+    } else {
+      return num;
+    }
+  }
   function setClock(selector, endtime) {
     const timer = document.querySelector(selector),
-      days = timer.querySelector('#days'),
+      days = timer.querySelector("#days"),
       hours = timer.querySelector('#hours'),
       minutes = timer.querySelector('#minutes'),
       seconds = timer.querySelector('#seconds'),
       timeInterval = setInterval(updateClock, 1000);
+    updateClock();
     function updateClock() {
       const t = getTimeRemaining(endtime);
-      days.innerHTML = t.days;
-      hours.innerHTML = t.hours;
-      minutes.innerHTML = t.minutes;
-      seconds.innerHTML = t.seconds;
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
       if (t.total <= 0) {
         clearInterval(timeInterval);
       }
